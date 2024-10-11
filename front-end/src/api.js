96e72ee342a29f294bbedd8ab114a172cce5105f
Syed Haris shah <haris.official222@gmail.com>
2024-10-11 23:49:40 +0500

    some changes in UI

---
+++ front-end/src/api.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:5000/todos';
+
+export const createTodo = (title) => axios.post(API_URL, { title });
+export const getTodos = () => axios.get(API_URL);
+
+export const updateTodo = (id, updatedTodo) =>
+  axios.put(`${API_URL}/${id}`, updatedTodo);
+
+export const deleteTodo = (id) => axios.delete(`${API_URL}/${id}`);
